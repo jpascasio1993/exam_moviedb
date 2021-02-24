03b93e477251890b85be3825e131d8eb060882ec
flux-macOS <jpascasio1993@gmail.com>
2021-02-24 09:58:50 +0800

    [UPDATE] refactored

--- app/src/main/java/com/exam/moviedb/data/domain/MovieDTO.kt
+++ app/src/main/java/com/exam/moviedb/data/domain/MovieDTO.kt
@@ -2,21 +2,22 @@ package com.exam.moviedb.data.domain
 
 import androidx.room.*
 import com.exam.moviedb.db.utils.ListIntDataConverter
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "movies")
 data class MovieDTO(
     @SerializedName("adult") @ColumnInfo(name = "adult") val adult: Boolean,
-    @SerializedName("backdrop_path") @ColumnInfo(name = "backdrop_path") val backdropPath: String,
-    @field:TypeConverters(ListIntDataConverter::class) @ColumnInfo(name = "genre_ids") val genreIds: List<Int> = listOf(),
+    @SerializedName("backdrop_path") @ColumnInfo(name = "backdrop_path") val backdropPath: String = "",
+    @Expose(serialize = true, deserialize = false) @field:TypeConverters(ListIntDataConverter::class) @ColumnInfo(name = "genre_ids") val genreIds: List<Int> = emptyList(),
     @SerializedName("id") @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = false) val id: Int,
-    @SerializedName("original_language") @ColumnInfo(name = "original_language") val originalLanguage: String,
-    @SerializedName("original_title") @ColumnInfo(name = "original_title") val originalTitle: String,
-    @SerializedName("overview") @ColumnInfo(name = "overview") val overview: String,
+    @SerializedName("original_language") @ColumnInfo(name = "original_language") val originalLanguage: String = "",
+    @SerializedName("original_title") @ColumnInfo(name = "original_title") val originalTitle: String = "",
+    @SerializedName("overview") @ColumnInfo(name = "overview") val overview: String = "",
     @SerializedName("popularity") @ColumnInfo(name = "popularity") val popularity: Int,
-    @SerializedName("poster_path") @ColumnInfo(name = "poster_path") val posterPath: String,
-    @SerializedName("release_date") @ColumnInfo(name = "release_date") val releaseDate: String,
-    @SerializedName("title") @ColumnInfo(name = "title") val title: String,
+    @SerializedName("poster_path") @ColumnInfo(name = "poster_path") val posterPath: String = "",
+    @SerializedName("release_date") @ColumnInfo(name = "release_date") val releaseDate: String = "",
+    @SerializedName("title") @ColumnInfo(name = "title") val title: String = "",
     @SerializedName("video") @ColumnInfo(name = "video") val video: Boolean,
     @SerializedName("vote_average") @ColumnInfo(name = "vote_average") val voteAverage: Float,
     @SerializedName("vote_count") @ColumnInfo(name = "vote_count") val voteCount: Int,
--- app/src/main/java/com/exam/moviedb/utils/gson/MovieDeserializer.kt
+++ app/src/main/java/com/exam/moviedb/utils/gson/MovieDeserializer.kt
@@ -6,41 +6,21 @@ import com.google.gson.*
 import java.lang.reflect.Type
 
 class MovieDeserializer : JsonDeserializer<List<MovieDTO>> {
-    private val gson: Gson = Gson()
+    private val gson = Gson()
     override fun deserialize(
         json: JsonElement?,
         typeOfT: Type?,
         context: JsonDeserializationContext?
     ): List<MovieDTO> {
         val jsonObject = json?.asJsonObject
-        Log.i("deserialize", "deserialize: ${jsonObject}")
         val jsonMovieResults = jsonObject?.getAsJsonArray("results")
         val page = jsonObject?.get("page")?.asInt ?: 0
-        val list = jsonMovieResults?.map { movieResults ->
-            movieResults.asJsonObject.let { it ->
-                // gson.fromJson(movieObject, MovieDTO::class.java).copy(page = page)
-                MovieDTO(
-                    it.get("adult").asBoolean,
-                    it.getNullable("backdrop_path")?.asString?: "",
-                    it.get("genre_ids").asJsonArray.map { it.asInt },
-                    it.get("id").asInt,
-                    it.getNullable("original_language")?.asString?:"",
-                    it.getNullable("original_title")?.asString?:"",
-                    it.getNullable("overview")?.asString?:"",
-                    it.get("popularity").asInt,
-                    it.getNullable("poster_path")?.asString?:"",
-                    it.getNullable("release_date")?.asString?:"",
-                    it.getNullable("title")?.asString?:"",
-                    it.get("video").asBoolean,
-                    it.get("vote_average").asFloat,
-                    it.get("vote_count").asInt,
-                    page
-                )
-
+            val list = jsonMovieResults?.map { movieResults ->
+                movieResults.asJsonObject.let { it ->
+                    gson.fromJson(it, MovieDTO::class.java).copy(genreIds = it.get("genre_ids").asJsonArray.map { it.asInt }, page = page)
+                }
             }
-
-        }
-        return list.orEmpty()
+            return list.orEmpty()
     }
 
 }
